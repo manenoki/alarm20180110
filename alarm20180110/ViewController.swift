f28e1401f91f85f517e03c142a558f529aab36b8
Hiejima <tatuyakun@katsuwlan170185.ed.tus.ac.jp>
2018-01-10 13:07:54 +0900

    Initial Commit

---
+++ alarm20180110/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  alarm20180110
+//
+//  Created by 比江嶋龍也 on 2018/01/10.
+//  Copyright © 2018年 比江嶋龍也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
